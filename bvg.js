a00a99e5c7c8982e1e1f72b8659d3afcbe27e017
Xavier Ho <contact@xavierho.com>
2015-03-27 10:34:24 +1000

    Added package.json for npm and separated LICENSE file

--- bvg.js
+++ bvg.js
@@ -12,6 +12,12 @@
   * Bindable Vector Graphics offers SVG elements that change as the data change,
   * and gives you tools to control their look.
   */
+
+// NodeJS amdefine patch
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
 define([], function () {
   var BVGIDCounter = 0;
 
@@ -517,27 +523,4 @@ define([], function () {
   * [Make a pull request](https://github.com/Spaxe/BVG.js/pulls) or
   * [post an issue](https://github.com/Spaxe/BVG.js/issues). Say hello to
   * contact@xaiverho.com.
-  */
-
-/**
-  * ### The MIT License (MIT)
-  * Copyright © 2015 Xavier Ho
-  *
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to deal
-  * in the Software without restriction, including without limitation the rights
-  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  * copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in all
-  * copies or substantial portions of the Software.
-  *
-  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  * SOFTWARE.
   */
