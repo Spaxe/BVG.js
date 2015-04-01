fcc98fa0ba7216dac9c609b4aee652e5acaa085b
Xavier Ho <contact@xavierho.com>
2015-04-02 02:46:00 +1100

    Fixed all examples

--- demo/002-poly.js
+++ demo/002-poly.js
@@ -10,14 +10,14 @@ require(['bvg'], function(BVG) {
   var data = [];
   var polyline = bvg.polyline(data);
   // Change its size based on mouse movement
-  bvg.addEventListener('mousemove', function (event) {
+  bvg.tag.addEventListener('mousemove', function (event) {
     if (event.buttons & 0x01) {
       var xy = [event.clientX, event.clientY];
       data.push(xy);
-      polyline.vertices(data);
+      polyline.data('points', data);
       bvg.polygon([[xy[0] - 25, xy[1] + 25], [xy[0] + 25, xy[1] + 25], [xy[0], xy[1] - 17]])
     } else if (event.buttons & 0x10) {
-      polyline.vertices([]);
+      polyline.data('points', []);
     } else {
       data = [];
       polyline = bvg.polyline(data);
